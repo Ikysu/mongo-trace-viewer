7a9c06bcd75668dd5f669f3c68a6343e0ca7f48c
ß <31385763+Ikysu@users.noreply.github.com>
2024-11-06 20:26:48 +0500

    Hide conn string

--- src/OV.Node/src/controllers/ConfigController.ts
+++ src/OV.Node/src/controllers/ConfigController.ts
@@ -13,7 +13,7 @@ class ConfigController {
            await MongoConfigStorage.save(configData);
            res.json({
             isConfigured: await MongoConnectionFactory.isInitialized(),
-            connectionString: configData?.connectionString
+            connectionString: ""
           });
         } else{
           res.status(400).json({
@@ -31,7 +31,7 @@ class ConfigController {
         
         res.json({
             isConfigured: await MongoConnectionFactory.isInitialized(),
-            connectionString: config.isConnectionStringEditLocked ? "" : config?.connectionString,
+            connectionString: "",
             isConnectionStringEditLocked: config.isConnectionStringEditLocked
         });
       } catch (error) {
